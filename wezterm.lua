3bc5214beda5686bd6fdc3a72ea12ad724712096
H1rono <hronok66@gmail.com>
2022-12-02 09:28:07 +0900

    :sparkles: Systemize font configuration in wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -17,6 +17,62 @@ local function file_exists(name)
     return res
 end
 
+-- fn() -> array<string>
+-- returns the sorted array of installed font names
+local function installed_fonts()
+    local cmd --[[string]] = "fc-list | awk -F ':' '{print $2}' | sort | uniq"
+    local handle --[[file]] = io.popen(cmd)  -- I'm not sure whether this type is correct
+    local cmd_res --[[string]] = handle:read("*a")
+    handle:close()
+    local --[[array<string>]] res = {}
+    for line --[[string]] in cmd_res:gmatch("[^\n\r]*") do
+        local l --[[string]] = line:gsub("^%s*(.-)%s*$", "%1")
+        res[#res + 1] = l
+    end
+    table.sort(res, function(a, b) return a < b end)
+    return res
+end
+
+-- fn(string, array<string>) -> boolean
+-- judges whether the font named `font_name` exists in the array `fonts`
+local function font_exists(font_name, fonts)
+    -- binary search
+    local ng --[[integer]], ok --[[integer]] = 0, #fonts
+    while ng + 1 < ok do
+        local mid --[[integer]] = (ng + ok) >> 1
+        if fonts[mid] < font_name then
+            ng = mid
+        else
+            ok = mid
+        end
+    end
+    return fonts[ok] ~= nil and font_name == fonts[ok]
+end
+
+-- fn(nil | array<string>, array<string>) -> array<string>
+local function get_font_fallback(candidates, all_fonts)
+    local res --[[array<string>]] = {}
+    for i --[[integer]] = 1, #candidates do
+        local cand --[[string]] = candidates[i]
+        if font_exists(cand, all_fonts) then
+            table.insert(res, cand)
+        end
+    end
+    return res
+end
+
+local function make_font()
+    local all_fonts --[[array<string>]] = installed_fonts()
+    local candidates --[[array<string>]] = {
+        -- my favorite fonts
+        "SF Mono",
+        "Menlo",
+        "FirgeNerd Console",  -- https://github.com/yuru7/Firge
+    }
+    local fallback --[[array<string>]] = get_font_fallback(candidates, all_fonts)
+    return wezterm.font_with_fallback(fallback)
+end
+
 -- type WezTermBackgroundLayer
 -- ... https://wezfurlong.org/wezterm/config/lua/config/background.html#layer-definition
 
@@ -59,7 +115,7 @@ end
 
 -- fn() -> array<WezTermBackgroundLayer>
 local function make_background()
-    local res = {
+    local res --[[array<WezTermBackgroundLayer>]] = {
         {   -- base background color
             source = { Color = "#282C34" },
             opacity = 1.0,
@@ -67,7 +123,14 @@ local function make_background()
             height = "100%",
         },
     }
-    local img = bg_image()
+    local img --[[nil | WezTermBackgroundLayer]] = bg_image {
+        home .. "/Pictures/bg.jpeg",
+        home .. "/Pictures/bg.jpg",
+        home .. "/.bg.jpeg",
+        home .. "/.bg.jpg",
+        xdg_config_home .. "/bg.jpeg",
+        xdg_config_home .. "/bg.jpg"
+    }
     if img ~= nil then
         res[2] = img
     end
@@ -75,11 +138,6 @@ local function make_background()
 end
 
 return {
-    font = wezterm.font_with_fallback {
-        -- my favorite fonts
-        'SF Mono',
-        'Menlo',
-        'FirgeNerd Console',  -- https://github.com/yuru7/Firge
-    },
+    font = make_font(),
     background = make_background()
 }

513937b08adf991ad2932313bbcb64cecb65cdc5
H1rono <hronok66@gmail.com>
2024-02-09 18:12:56 +0900

    :sparkles: Add default args in cargo-clean-all.nix

--- packages/cargo-clean-all.nix
+++ packages/cargo-clean-all.nix
@@ -1,8 +1,8 @@
 # https://github.com/dnlmlr/cargo-clean-all/releases/tag/v0.6.2
-{ pkgs
-, lib
-, rustPlatform
-, src
+{ pkgs ? import <nixpkgs> { }
+, lib ? pkgs.lib
+, rustPlatform ? pkgs.rustPlatform
+, src ? import ../nix/fetchFlakeInput.nix "cargo-clean-all"
 }:
 let
   info = import ../nix/readInputInfo.nix "cargo-clean-all";

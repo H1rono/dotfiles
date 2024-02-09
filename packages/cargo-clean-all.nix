e19c179c3336ba94b322581a13a93e8f13607a94
H1rono <hronok66@gmail.com>
2024-02-09 18:12:04 +0900

    :fire: Remove unused args in cargo-clean-all

--- packages/cargo-clean-all.nix
+++ packages/cargo-clean-all.nix
@@ -1,18 +1,12 @@
 # https://github.com/dnlmlr/cargo-clean-all/releases/tag/v0.6.2
 { pkgs
 , lib
-, stdenv
-, fetchFromGitHub
 , rustPlatform
-, coreutils
-, bash
-, direnv
-, openssl
 , src
 }:
 let
   info = import ../nix/readInputInfo.nix "cargo-clean-all";
-  inherit (info.original) owner ref repo;
+  inherit (info.original) ref repo;
 in
 rustPlatform.buildRustPackage {
   inherit src;
